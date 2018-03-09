4c02d973d0b08ae4b8f77ea582c23705b1ae6259
CSA-KJC <33467695+CSA-KJC@users.noreply.github.com>
2018-03-09 15:51:42 -0600

    Update longest_word.py

--- longest_word.py
+++ longest_word.py
@@ -2,19 +2,21 @@
 #Computer Programming 12th
 #March 6, 2018
 num=0
+word=""
 
-def words(num):
+def words(num,word):
     files=open("alice.txt","r")
-    new=files.readline()
-    while num>count and new:
-        count=len(x)
-        print(count)
-        if count>num:
-            num=count
-            print(num)
-            print(x)
-        new=files.readline()
+    new=files.readlines()
+    for y in new:
+        z=y.split()
+        for x in z:
+            count=len(x)
+            if count>num:
+                num=count
+                word=x
+    print(num)
+    print(word)
                        
     files.close()
 
-words(num)
+words(num,word)
