048ffc32eb5009e93d04cf932cc43e81ee9348ff
CSA-KJC <33467695+CSA-KJC@users.noreply.github.com>
2018-03-09 15:56:21 -0600

    Update counting.py

--- counting.py
+++ counting.py
@@ -2,13 +2,13 @@
 #Computer Programming 12th
 #March 5, 2018
 
-word=input("Which word would you like to use: ")
+word=input("Which word would you like to use: ") #asks user for input
 
 def count(word):
     num={}
-    for y in word:
-        letter=word.count(y)
-        num.update({y:letter})
+    for y in word: #runs through each letter
+        letter=word.count(y) #counts the number of specific letters
+        num.update({y:letter}) #adds it to dictionary
     print(num)
 
 count(word)
