b5044d6448c850a17725f28f233c449235d6ca91
CSA-KJC <33467695+CSA-KJC@users.noreply.github.com>
2018-03-09 15:55:13 -0600

    Update longest_word.py

--- longest_word.py
+++ longest_word.py
@@ -5,13 +5,13 @@ num=0
 word=""
 
 def words(num,word):
-    files=open("alice.txt","r")
+    files=open("alice.txt","r") #opens file
     new=files.readlines()
-    for y in new:
-        z=y.split()
-        for x in z:
-            count=len(x)
-            if count>num:
+    for y in new: #runs through the file
+        z=y.split() #splits into word
+        for x in z: #Runs through each word
+            count=len(x) #Counts amount of letters
+            if count>num: #updates original amount to new one if new word it is longer
                 num=count
                 word=x
     print(num)
